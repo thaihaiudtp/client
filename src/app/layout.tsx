6fbfdabf308d21dd922fbec167244600f2cbcb04
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-01-28 02:06:09 +0700

    hix3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Easy English",
@@ -18,6 +18,7 @@ export default function RootLayout({
        className="bg-gray-100"
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
