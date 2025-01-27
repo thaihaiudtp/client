b602dd453c88b978870174d005e7872180f8e383
thaihaiudtp <nguyenmanhthaiha@gmail.com>
2025-01-28 02:01:23 +0700

    hix2

--- src/app/admin/test/[id]/page.tsx
+++ src/app/admin/test/[id]/page.tsx
@@ -139,7 +139,7 @@ export default function OneTestPage(){
           }
           }
         fetchClass();
-      },[testId])
+      },[testId, token, router])
     return(
         <>
             {loading ? (
